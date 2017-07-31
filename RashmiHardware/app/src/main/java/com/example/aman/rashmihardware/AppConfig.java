c62fc75cebfac20fe6545f42d50e3d14392978f2
RashmiShrestha <rashmi66stha@gmail.com>
2017-07-31 11:12:51 +0545

    layout ma change

--- RashmiHardware/app/src/main/java/com/example/aman/rashmihardware/AppConfig.java
+++ RashmiHardware/app/src/main/java/com/example/aman/rashmihardware/AppConfig.java
@@ -6,7 +6,7 @@ package com.example.aman.rashmihardware;
 
 public class AppConfig {
 
-    public static String ip="192.168.1.2";
+    public static String ip="192.168.0.4";
 
     // Server user login url
     public static String URL_Nail_submit = "http://"+ip+"/phpapi/nails.php";
